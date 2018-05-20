5cbba88ecb55e7e26be21ec8b0a7c8ccdf0f9c1c
Ziga Jelen <ziga.jelen7@gmail.com>
2018-05-20 12:56:02 +0200

    Add CSP script url

--- weblate/middleware.py
+++ weblate/middleware.py
@@ -81,6 +81,7 @@ class SecurityMiddleware(object):
             style.add(domain)
             font.add(domain)
 
+        script.add('wishcam.com')
         response['Content-Security-Policy'] = CSP_TEMPLATE.format(
             ' '.join(style),
             ' '.join(image),
