b395dabc493cc9b5e5e87c6be053ee40d8b137e9
Ziga Jelen <ziga.jelen7@gmail.com>
2018-05-20 13:04:27 +0200

    Add custom.js script

---
+++ weblate/static/js/custom.js
@@ -0,0 +1,24 @@
+$('#dropdownMenuUpload').click(function () {
+
+    // Send the data using post
+    var posting = $.post('https://wishcam.com/update-pot', {
+        component: '{{ object.subproject.slug }}',
+        lang: '{{ object.language.code }}'
+    });
+    $(this).html(
+        '<i class="fa fa-refresh fa-spin fa-fw"></i>' +
+        '<span class="sr-only">Loading...</span>' +
+        'Nalagam...');
+
+    $(this).attr("disabled", true);
+
+    // Put the results in a div
+    posting.done(function (data) {
+        if (data.status) {
+            $('#dropdownMenuUpload').html('<i class="fa fa-check" aria-hidden="true"></i> Uspešno posodobljeno');
+        } else {
+            $('#dropdownMenuUpload').html('<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Napaka:' + data.error);
+        }
+    });
+
+});
